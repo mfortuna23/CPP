87f9d6834ac8035bd813ac7ffe0ed10ed8ed13d0
mfortuna <mfortuna@student.42porto.com>
2025-06-04 14:36:46 +0100

    flagtrap

--- CPP03/ex02/FlagTrap.cpp
+++ CPP03/ex02/FlagTrap.cpp
@@ -6,13 +6,14 @@
 /*   By: mfortuna <mfortuna@student.42porto.com>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/06/04 12:09:57 by mfortuna          #+#    #+#             */
-/*   Updated: 2025/06/04 13:59:08 by mfortuna         ###   ########.fr       */
+/*   Updated: 2025/06/04 14:31:08 by mfortuna         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "FlagTrap.hpp"
 
 FlagTrap::FlagTrap(){
+	std::cout << CYAN << "FlagTrap: Default constructor called" << RESET << std::endl;
 	name = "";
 	hitPoints = 100;
 	energyPoints = 100;
@@ -20,6 +21,7 @@ FlagTrap::FlagTrap(){
 }
 
 FlagTrap::FlagTrap(std::string newName){
+	std::cout << CYAN << "FlagTrap: String constructor called" << RESET << std::endl;
 	name = newName;
 	hitPoints = 100;
 	energyPoints = 100;
@@ -27,10 +29,12 @@ FlagTrap::FlagTrap(std::string newName){
 }
 
 FlagTrap::FlagTrap(const FlagTrap& other) : ClapTrap(other){
+	std::cout << CYAN << "FlagTrap: Copy constructor called" << RESET << std::endl;
 	*this = other;
 }
 
 FlagTrap &FlagTrap::operator= (const FlagTrap& other){
+	std::cout << CYAN << "FlagTrap: Copy assignment operator called" << RESET << std::endl;
 	name = other.getName();
 	hitPoints = other.getHitPoints();
 	energyPoints = other.getEnergyPoints();
@@ -38,6 +42,6 @@ FlagTrap &FlagTrap::operator= (const FlagTrap& other){
 	return(*this);
 }
 
-FlagTrap::~FlagTrap {
-	std::cout << BLUE << "FlagTrap"
+FlagTrap::~FlagTrap() {
+	std::cout << BLUE << "FlagTrap: Destructor called" << RESET << std::endl;
 }
